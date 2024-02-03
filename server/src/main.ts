0416a7f84e4397648c39dadc1ab7da8ee4fe2b9a
a-liut <andrea.liut@gmail.com>
2024-02-03 22:40:57 +0100

    added cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import BagService from './BagService';
 import express from 'express';
 import bodyParser from 'body-parser';
+var cors = require('cors');
 import Bag from './model/Bag';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const bagService = new BagService();
 
@@ -25,6 +27,18 @@ app.post('/bags', function (req, res) {
 });
 
 app.get('/bags/:bag_name', function (req, res) {
+  const bagName = req.params.bag_name;
+
+  const bag = bagService.getBag(bagName);
+
+  if (!bag) {
+    res.sendStatus(404);
+  } else {
+    res.send(bag);
+  }
+});
+
+app.get('/bags/:bag_name/potato_list', function (req, res) {
   const bagName = req.params.bag_name;
 
   const bag = bagService.getBag(bagName);
